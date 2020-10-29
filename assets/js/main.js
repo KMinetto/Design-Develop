ed0dc9c0fb5396ad4cb79fd03257a4d90b55dd95
KMinetto <killian.minetto@gmail.com>
2020-10-29 10:44:22 +0100

    First Commit and add Header

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+let ul = document.querySelector('ul');
+let li = document.querySelectorAll('li');
+
+li.forEach(el => {
+    el.addEventListener('click', () => {
+        ul.querySelector('.active').classList.remove('active');
+
+        el.classList.add('active');
+    })
+})
